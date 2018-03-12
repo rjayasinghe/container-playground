ad2875c35f2bc9bca55b07bec7bd21cc837d5cac
Robin de Silva Jayasinghe <jayasinghe@synyx.de>
2018-03-12 09:37:24 +0100

    start to connect stacker and container area

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ func main() {
 	containerAreaClearance := make(chan bool)
 
 	stacker := domain.Stacker{Name : "stacker 01" }
+	containerArea := domain.NewContainerArea(10, "yolo-area")
+
+	go containerArea.StartWork(requestContainerSpace, containerAreaClearance, containerToContainerArea)
+
 	go stacker.StartWork(containerToStacker, rejectedContainer, containerToContainerArea, requestContainerSpace, containerAreaClearance)
 
 	<- forever
